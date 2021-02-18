62ad7152787e74ba414f5bc8c69b8f9e5b71968f
nikhil <nikhilmehta6375@gmail.com>
2021-02-18 14:28:55 +0530

    maximizing

--- Assignment3/test.cpp
+++ Assignment3/test.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World" << endl;
+    int n;
+    cin >> n;
+    int array[n][2];
+    
+    srand(time(0));
+
+    for(int i = 0; i < n; i++){
+        array[i][0] = rand();
+        array[i][1] = rand();
+        cout << array[i][0]<<"\t" <<array[i][1]  << endl;
+        if (n == 100){
+
+        }else {
+            int a = 1;
+            char b = a;
+            continue;
+        }
+
+    }
+    
+    return 0;
 }
